2e0acf5c96a7e130d996fdf846b73cb6218609b9
Tadynas <griciustadas@gmail.com>
2018-09-28 18:20:51 +0300

    Sutvarkytas login langas

---
+++ VirtualLibraryApp/Login.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace VirtualLibraryApp
+{
+	partial class Login
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.PasswordTextBox = new System.Windows.Forms.TextBox();
+			this.SignInButton = new System.Windows.Forms.Button();
+			this.UsernameTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(333, 50);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(143, 44);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "LOGIN";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(277, 134);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(73, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Username";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(277, 172);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(69, 17);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Password";
+			// 
+			// PasswordTextBox
+			// 
+			this.PasswordTextBox.Location = new System.Drawing.Point(366, 172);
+			this.PasswordTextBox.Name = "PasswordTextBox";
+			this.PasswordTextBox.Size = new System.Drawing.Size(176, 22);
+			this.PasswordTextBox.TabIndex = 4;
+			this.PasswordTextBox.TextChanged += new System.EventHandler(this.PasswordTextBox_TextChanged);
+			// 
+			// SignInButton
+			// 
+			this.SignInButton.Location = new System.Drawing.Point(280, 223);
+			this.SignInButton.Name = "SignInButton";
+			this.SignInButton.Size = new System.Drawing.Size(262, 48);
+			this.SignInButton.TabIndex = 5;
+			this.SignInButton.Text = "Sign In";
+			this.SignInButton.UseVisualStyleBackColor = true;
+			this.SignInButton.Click += new System.EventHandler(this.SignInButton_Click_1);
+			// 
+			// UsernameTextBox
+			// 
+			this.UsernameTextBox.Location = new System.Drawing.Point(366, 131);
+			this.UsernameTextBox.Name = "UsernameTextBox";
+			this.UsernameTextBox.Size = new System.Drawing.Size(176, 22);
+			this.UsernameTextBox.TabIndex = 6;
+			this.UsernameTextBox.TextChanged += new System.EventHandler(this.UsernameTextBox_TextChanged);
+			// 
+			// Login
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.UsernameTextBox);
+			this.Controls.Add(this.SignInButton);
+			this.Controls.Add(this.PasswordTextBox);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Login";
+			this.Text = "Login";
+			this.Load += new System.EventHandler(this.Login_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox PasswordTextBox;
+		private System.Windows.Forms.Button SignInButton;
+		private System.Windows.Forms.TextBox UsernameTextBox;
+	}
+}
---
+++ VirtualLibraryApp/Login.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace VirtualLibraryApp
+{
+	public partial class Login : Form
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
+
+		private void Login_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void UsernameTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void SignInButton_Click_1(object sender, EventArgs e)
+		{
+			//sql serverio radimas. "System.IO.Directory.GetParent(@"../ ").FullName" randa projecto adresa kompiuteryje 
+			//pvz.: "C:\Users\Tadas\Desktop\VirtualLibraryApp\bin" ir tada projekte randa duomenu faila, 
+			//kur yra visi vartotoju vardai ir slaptazodziai.
+			SqlConnection sqlConnect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" +
+														System.IO.Directory.GetParent(@"../ ").FullName + 
+														@"\Data\userdata.mdf;Integrated Security=True;Connect Timeout=30");
+
+			//Tikriname ar ivesti duomenis atitinka su sql duomenu bazeje esanciais vartotojo duomenimis.
+			SqlDataAdapter sqlData = new SqlDataAdapter("SELECT COUNT(*) FROM login WHERE username = '" + 
+														UsernameTextBox.Text + "' AND password = '" + 
+														PasswordTextBox.Text + "'", sqlConnect);
+
+			//Sukuriame nauja duomenu lentele ir ja uzpildome skaiciu 1(jei vartotojas ivede teisingus duomenis) arba 0(jei duomenys neteisingi).
+			DataTable data = new DataTable();
+			sqlData.Fill(data);
+
+			//Jei vartotojo ivestas vardas ir slaptazodis atitiko, atidaromas meniu langas, kitu atveju lentele kad duomenys blogi.
+			if (data.Rows[0][0].ToString() == "1")
+			{
+				this.Hide();
+				Main mainMenu = new Main();
+				mainMenu.Show();
+			}
+			else
+			{
+				MessageBox.Show("Incorrect Username or Password. Please, try again.");
+			}
+		}
+	}
+}
