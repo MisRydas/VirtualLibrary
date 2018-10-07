5f24503396f77fc46de41ba86b81a0ae532a58f4
Tadynas <griciustadas@gmail.com>
2018-10-07 21:39:08 +0300

    Added sample book.sql file

---
+++ DB Tables/Books.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `Books` (
+   `Id` int(11) NOT NULL AUTO_INCREMENT,	
+   `ISBN13` varchar(100) NOT NULL,
+   `ISBN10` varchar(100) NULL,
+   `BookName` varchar(100) NOT NULL,
+   `Author` varchar(100) NOT NULL, 
+   `Publisher` varchar(100) NULL,   
+   `Published` datetime NOT NULL,
+   `ListPrice` double NULL,
+   `CoverLink` varchar(100) NULL,   
+   PRIMARY KEY (`Id`),
+   UNIQUE KEY `BOOKS-ISBN` (`ISBN13`)
+);
