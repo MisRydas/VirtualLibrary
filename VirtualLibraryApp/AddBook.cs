f236f67d25a2c92744ad07f10bc845ea50cde5cf
Tadynas <griciustadas@gmail.com>
2018-10-19 12:34:23 +0300

    added regex function to adding book information

--- VirtualLibraryApp/AddBook.cs
+++ VirtualLibraryApp/AddBook.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace VirtualLibraryApp
 {
 	public partial class AddBook : Form
 	{
+
+		Regex regex;
+
 		public AddBook()
 		{
 			InitializeComponent();
@@ -21,10 +18,28 @@ namespace VirtualLibraryApp
 		{
 			String bookName = BookNameBox.Text;
 			String ISBN13 = ISBN13Box.Text;
+			regex = new Regex("^[0-9]{13}");
+			if(!regex.IsMatch(ISBN13))
+			{
+				MessageBox.Show("Wrong ISBN-13 Code. No spaces, letters, punctuations and code must have 13 numbers. Please correct it.");
+				return;
+			}
 			String ISBN10 = ISBN10Box.Text;
+			regex = new Regex("^[0-9]{10}");
+			if (!regex.IsMatch(ISBN10))
+			{
+				MessageBox.Show("Wrong ISBN-10 Code. No spaces, letters, punctuations and code must have 13 numbers. Please correct it.");
+				return;
+			}
 			String author = AuthorBox.Text;
 			String publisher = PublisherBox.Text;
 			int.TryParse(PublishedBox.Text, out int published);
+			regex = new Regex("^[0-9]{4}");
+			if (!regex.IsMatch(published.ToString()))
+			{
+				MessageBox.Show("Wrong Published Date format. Please correct it. For example: 2018");
+				return;
+			}
 			double.TryParse(ListPriceBox.Text, out double listPrice);
 			String coverLink = BookCoverLinkBox.Text;
 
