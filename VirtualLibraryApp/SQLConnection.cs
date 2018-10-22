200e90312e1074da990865b342fd379e46cd169e
Tomas Ukrinas <tomas.ukrinas1997@gmail.com>
2018-10-22 22:49:43 +0300

    New extension class added

--- VirtualLibraryApp/SQLConnection.cs
+++ VirtualLibraryApp/SQLConnection.cs
@@ -3,46 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.Data;
+using SQLConnectionExtensions;
 
 namespace VirtualLibraryApp
 {
     class SQLConnection
     {
-        static String server = "78.61.151.149";
-        static String database = "LibraryAppDB";
-        static String uid = "LibraryAppUser";
-        static String password = "AudiBMW";
-        static String connectionString = "SERVER=" + server + ";" + "DATABASE=" +
-        database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";Sslmode=none;";
-
-        private static MySqlConnection Connect()
-        {
-            MySqlConnection conn = new MySqlConnection(connectionString);
-            conn.Open();
-            return conn;
-        }
-
-        public static void Query(String query) //Paprastas metodas INSERT, DELETE sakiniams vykdyti, negražina rezultato.
-        {
-            MySqlConnection conn = SQLConnection.Connect();
-            MySqlCommand command = new MySqlCommand(query, conn);
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-
-        public static DataTable SelectQuery(String query) //Metodas SELECT sakiniams vykdyti, nes gražina reikšme supildytą į DataTable
-        {
-            MySqlConnection conn = SQLConnection.Connect();
-            MySqlCommand command = new MySqlCommand(query, conn);
-            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
-            MyAdapter.SelectCommand = command;
-            DataTable dTable = new DataTable();
-            MyAdapter.Fill(dTable);
-            conn.Close();
-            return dTable;
-        }
 
         public static User GetUserById(int id)
         {
---
+++ VirtualLibraryApp/SQLConnectionExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace SQLConnectionExtensions
+{
+    static class SQLConnectionExtension
+    {
+        static String server = "78.61.151.149";
+        static String database = "LibraryAppDB";
+        static String uid = "LibraryAppUser";
+        static String password = "AudiBMW";
+        static String connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+        database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";Sslmode=none;";
+
+        private static MySqlConnection Connect()
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+
+        public static void Query(this String query) //Paprastas metodas INSERT, DELETE sakiniams vykdyti, negražina rezultato.
+        {
+            MySqlConnection conn = Connect();
+            MySqlCommand command = new MySqlCommand(query, conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public static DataTable SelectQuery(this String query) //Metodas SELECT sakiniams vykdyti, nes gražina reikšme supildytą į DataTable
+        {
+            MySqlConnection conn = Connect();
+            MySqlCommand command = new MySqlCommand(query, conn);
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = command;
+            DataTable dTable = new DataTable();
+            MyAdapter.Fill(dTable);
+            conn.Close();
+            return dTable;
+        }
+
+    }
+}
