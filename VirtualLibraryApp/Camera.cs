8f74d88000ed430b5611210eb821019b71e3ff28
Tadynas <griciustadas@gmail.com>
2018-10-23 18:14:24 +0300

    Updated camera UI positions

--- VirtualLibraryApp/Camera.cs
+++ VirtualLibraryApp/Camera.cs
@@ -12,42 +12,39 @@ namespace VirtualLibraryApp
 	public partial class CameraScreen : Form
 	{
 
-        public User User { get; set; }
+		public User User { get; set; }
 
-        Camera myCamera = new Camera();
+		Camera myCamera = new Camera();
+
 		public CameraScreen(User User)
 
-		User User;
-
-		public Camera(User User)
-
 		{
-            this.User = User;
+			this.User = User;
 			InitializeComponent();
 
-            GetInfo();
-            myCamera.OnFrameArrived += myCamera_OnFrameArrived;
+			GetInfo();
+			myCamera.OnFrameArrived += myCamera_OnFrameArrived;
 		}
-        private void GetInfo()
-        {
-            var myCameraResolutions = myCamera.GetSupportedResolutions();
+		private void GetInfo()
+		{
+			var myCameraResolutions = myCamera.GetSupportedResolutions();
 
-            foreach (var r in myCameraResolutions)
-                CameraResolutionComboBox.Items.Add(r);
+			foreach (var r in myCameraResolutions)
+				CameraResolutionComboBox.Items.Add(r);
 
-            CameraResolutionComboBox.SelectedIndex = 0;
-        }
-        private void myCamera_OnFrameArrived(object source, FrameArrivedEventArgs e)
-        {
-            Image img = e.GetFrame();
-            BarcodeImageBox.Image = img;
+			CameraResolutionComboBox.SelectedIndex = 0;
+		}
+		private void myCamera_OnFrameArrived(object source, FrameArrivedEventArgs e)
+		{
+			Image img = e.GetFrame();
+			BarcodeImageBox.Image = img;
 
-        }
-        private void LoadBarcodeButton_Click(object sender, EventArgs e)
+		}
+		private void LoadBarcodeButton_Click(object sender, EventArgs e)
 		{
 			//Atidaromas aplankas, kuriame yra barcode pavyzdziai ir pasirinkus barcod'a jis yra uzkraunamas.
 			OpenFileDialog OD = new OpenFileDialog();
-            OD.InitialDirectory = System.IO.Directory.GetParent(@"../../../ ").FullName + @"\Barcode Images";
+			OD.InitialDirectory = System.IO.Directory.GetParent(@"../../../ ").FullName + @"\Barcode Images";
 			if (OD.ShowDialog() == DialogResult.OK)
 				BarcodeImageBox.Load(OD.FileName);
 
@@ -76,8 +73,8 @@ namespace VirtualLibraryApp
 			//ieskom knygos su isbn
 			//	bookdata = SQLConnection.SelectQuery("SELECT * FROM Books WHERE ISBN" + isbnLength + " = '" + isbn + "';");
 
-            
-            DataTable bookData = GetAllBooksInDataTable();
+
+			DataTable bookData = GetAllBooksInDataTable();
 
 			var bookInformation = from book in bookData.AsEnumerable() where book.Field<string>("ISBN" + isbnLength) == isbn select book;
 
@@ -98,47 +95,46 @@ namespace VirtualLibraryApp
 			}
 		}
 
-        private void ScanButton_Click(object sender, EventArgs e)
-        {
-            //skenuoja knygos barcode tuo metu, jei jo nepagauna, nemeta jokio erroro ir reik spaust vel
-            try
-            {
-                //Nuskenuojami barcodo duomenis ir gaunamas knygos isbn kodas.
-                BarcodeDecoder Scanner = new BarcodeDecoder();
-                Result result = Scanner.Decode(new Bitmap(BarcodeImageBox.Image));
-                //Atidaromas tinklapis su informacija apie ieskoma knyga.(Tik kaip pavyzdys, vėliau bus pakeista).
-                //System.Diagnostics.Process.Start("https://isbnsearch.org/isbn/" + result.Text);
+		private void ScanButton_Click(object sender, EventArgs e)
+		{
+			//skenuoja knygos barcode tuo metu, jei jo nepagauna, nemeta jokio erroro ir reik spaust vel
+			try
+			{
+				//Nuskenuojami barcodo duomenis ir gaunamas knygos isbn kodas.
+				BarcodeDecoder Scanner = new BarcodeDecoder();
+				Result result = Scanner.Decode(new Bitmap(BarcodeImageBox.Image));
+				//Atidaromas tinklapis su informacija apie ieskoma knyga.(Tik kaip pavyzdys, vėliau bus pakeista).
+				//System.Diagnostics.Process.Start("https://isbnsearch.org/isbn/" + result.Text);
 
 
-                //Tikrina ISBN formatą ar isbn13 ar isbn10, jei nei vienas, tai rastas blogas kodas.
-                if (result.Text.Length == 13)
-                {
-                    CheckISBN(result.Text, 13);
-                }
-                else if (result.Text.Length == 10)
-                {
-                    CheckISBN(result.Text, 10);
-                }
-                else
-                {
-                    MessageBox.Show("Wrong ISBN, please try again.");
-                }
-            }
-            catch (MessagingToolkit.Barcode.NotFoundException)
-            {
-            }
-        }
+				//Tikrina ISBN formatą ar isbn13 ar isbn10, jei nei vienas, tai rastas blogas kodas.
+				if (result.Text.Length == 13)
+				{
+					CheckISBN(result.Text, 13);
+				}
+				else if (result.Text.Length == 10)
+				{
+					CheckISBN(result.Text, 10);
+				}
+				else
+				{
+					MessageBox.Show("Wrong ISBN, please try again.");
+				}
+			}
+			catch (MessagingToolkit.Barcode.NotFoundException)
+			{
+			}
+		}
 
-        private void CameraResolutionComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            myCamera.Start(CameraResolutionComboBox.SelectedIndex);
-        }
+		private void CameraResolutionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			myCamera.Start(CameraResolutionComboBox.SelectedIndex);
+		}
 
-        private void CameraScreen_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            myCamera.Stop();
-        }
-    }
+		private void CameraScreen_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			myCamera.Stop();
+		}
 
 		private void Back_Click(object sender, EventArgs e)
 		{
@@ -147,5 +143,4 @@ namespace VirtualLibraryApp
 			mainMenu.Show();
 		}
 	}
-
 }
