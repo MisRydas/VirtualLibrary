3c5e21e9089b40ef0a9f99515e2b28c2768a2150
Tomas Ukrinas <tomas.ukrinas1997@gmail.com>
2018-10-31 19:46:03 +0200

    Pakeitimai padaryti paskaitoje, atsiskaitant pirmąją užduotį.

--- VirtualLibraryApp/SQLConnection.cs
+++ VirtualLibraryApp/SQLConnection.cs
@@ -9,13 +9,21 @@ namespace VirtualLibraryApp
 {
     public class SQLConnection
     {
+        private static IDBConnection conn = null;
+        private static IDBConnection Conn { get
+            {
+                if (conn == null)
+                {
+                    conn = new MySQLConnection();
+                }
+                return conn;
+            } }
 
         public static User GetUserById(int id)
         {
             User user = new User();
             string query = String.Format("SELECT * FROM Users WHERE Id = {0};", id);
-            IDBConnection conn = new MySQLConnection();
-            DataTable dt = conn.SelectQuery(query);
+            DataTable dt = Conn.SelectQuery(query);
             user.Id = (int)dt.Rows[0]["Id"];
             user.UserName = (string)dt.Rows[0]["UserName"];
             if (dt.Rows[0]["FirstName"] != DBNull.Value) user.FirstName = (string)dt.Rows[0]["FirstName"];
@@ -27,15 +35,13 @@ namespace VirtualLibraryApp
         public static void AddNewUser(String UserName, String Password, String FirstName, String LastName)
         {
             string query = String.Format("Insert into Users (UserName, Password, FirstName, LastName) VALUES ('{0}', '{1}', '{2}', '{3}');;", UserName, Password, FirstName, LastName);
-            IDBConnection conn = new MySQLConnection();
-            conn.Query(query);
+            Conn.Query(query);
         }
 
         public static void AddISBNToHistory(int UserId, String ISBN)
         {
             string query = String.Format("Insert into ISBNSearches (UserId, ISBN) VALUES ('{0}', '{1}');;", UserId, ISBN);
-            IDBConnection conn = new MySQLConnection();
-            conn.Query(query);
+            Conn.Query(query);
         }
 
 		public static void AddNewBook(String BookName, String ISBN13, String ISBN10, String Author, String Genre, String Publisher, int Published, double ListPrice, String CoverLink)
@@ -43,29 +49,25 @@ namespace VirtualLibraryApp
 			string query = String.Format("Insert into Books (BookName, ISBN13, ISBN10, Author, Publisher, Published, ListPrice, CoverLink, Genre) VALUES " +
 										"('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}');;",
 										BookName, ISBN13, ISBN10, Author, Publisher, Published, ListPrice, CoverLink, Genre);
-            IDBConnection conn = new MySQLConnection();
-            conn.Query(query);
+            Conn.Query(query);
         }
 
         public static DataTable GetAllBooksInDataTable()
         {
             String query = "Select * FROM Books;";
-            IDBConnection conn = new MySQLConnection();
-            return conn.SelectQuery(query);
+            return Conn.SelectQuery(query);
         }
 
         public static DataTable GetAllUsersInDataTable()
         {
             String query = "Select * FROM Users;";
-            IDBConnection conn = new MySQLConnection();
-            return conn.SelectQuery(query);
+            return Conn.SelectQuery(query);
         }
 
         public static DataTable GetLastSearches(int userId)
         {
             String query = String.Format("SELECT b.ISBN13, b.CoverLink FROM ISBNSearches s JOIN Books b ON s.ISBN = b.ISBN13 WHERE s.UserId = {0} ORDER BY s.Time DESC;", userId.ToString());
-            IDBConnection conn = new MySQLConnection();
-            return conn.SelectQuery(query);
+            return Conn.SelectQuery(query);
         }
 
 		public static DataTable GetAllMissingBooksISBN()
@@ -76,15 +78,13 @@ namespace VirtualLibraryApp
 									    "LENGTH(S.ISBN) = 10 AND S.ISBN NOT IN (SELECT ISBN10 FROM Books) " +
 										"GROUP BY ISBN " +
 									    "ORDER BY S.Time DESC;");
-            IDBConnection conn = new MySQLConnection();
-            return conn.SelectQuery(query);
+            return Conn.SelectQuery(query);
 		}
 
 		public static DataView GetBookByISBNInDataView(string isbn)
         {
             String query = String.Format("SELECT * FROM Books WHERE ISBN13 = {0};", isbn);
-            IDBConnection conn = new MySQLConnection();
-            return conn.SelectQuery(query).AsDataView();
+            return Conn.SelectQuery(query).AsDataView();
         }
     }
 }
