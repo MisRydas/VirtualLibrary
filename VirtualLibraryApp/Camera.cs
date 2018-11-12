1c1eb99be4524ebd257fcf5d050ee4ee3f043f57
Efkinatorius <36881430+Efkinatorius@users.noreply.github.com>
2018-11-12 20:00:07 +0200

    delegates

--- VirtualLibraryApp/Camera.cs
+++ VirtualLibraryApp/Camera.cs
@@ -9,7 +9,8 @@ using DarrenLee.Media;
 
 namespace VirtualLibraryApp
 {
-	public partial class CameraScreen : Form
+    delegate void Check(int i, string s);
+    public partial class CameraScreen : Form
 	{
 
 		public User User { get; set; }
@@ -17,7 +18,7 @@ namespace VirtualLibraryApp
 		Camera myCamera = new Camera();
 
         //sukuriamas delegatas
-        private delegate void Check1013(string s, int i);
+        //private delegate void Check1013(string s, int i);
         public CameraScreen(User User)
 
 		{
@@ -49,76 +50,52 @@ namespace VirtualLibraryApp
 			//Atidaromas aplankas, kuriame yra barcode pavyzdziai ir pasirinkus barcod'a jis yra uzkraunamas.
 			OpenFileDialog OD = new OpenFileDialog();
 			OD.InitialDirectory = System.IO.Directory.GetParent(@"../../../ ").FullName + @"\Barcode Images";
-			if (OD.ShowDialog() == DialogResult.OK)
-				BarcodeImageBox.Load(OD.FileName);
+            if (OD.ShowDialog() == DialogResult.OK)
+                BarcodeImageBox.Load(OD.FileName);
 
 			//Nuskenuojami barcodo duomenis ir gaunamas knygos isbn kodas.
 			BarcodeDecoder Scanner = new BarcodeDecoder();
 			Result result = Scanner.Decode(new Bitmap(BarcodeImageBox.Image));
 
-            //instantiate'inam delegata naudodami anonymous method'a
-            Check1013 ch1013 = delegate (string text, int length)
-            {
-                if (length == 13)
-                {
-                CheckISBN(text, 13);
-                }
-                else if (length == 10)
-                {
-                CheckISBN(text, 10);
-                }
-                else
-                {
-                MessageBox.Show("Wrong ISBN, please try again.");
-                }
-            };
+            Check ch1013 = new Check(Check1013);
+
             //iskvieciam delegata
-            ch1013(result.Text, result.Text.Length);
-            
-			//Tikrina ISBN formatą ar isbn13 ar isbn10, jei nei vienas, tai rastas blogas kodas.
-			//if (result.Text.Length == 13)
-			//{
-			//	CheckISBN(result.Text, 13);
-			//}
-			//else if (result.Text.Length == 10)
-			//{
-			//	CheckISBN(result.Text, 10);
-			//}
-			//else
-			//{
-			//	MessageBox.Show("Wrong ISBN, please try again.");
-			//}
-		}
+            ch1013(result.Text.Length, result.Text);
+        }
 
-		//Tikrinam isbn ar yra tokia knyga duombazej
-		private void CheckISBN(string isbn, int isbnLength)
-		{
-			//ieskom knygos su isbn
-			//	bookdata = SQLConnection.SelectQuery("SELECT * FROM Books WHERE ISBN" + isbnLength + " = '" + isbn + "';");
-			SQLConnection.AddISBNToHistory(User.Id, isbn);
-
-			DataTable bookData = GetAllBooksInDataTable();
-
-			var bookInformation = from book in bookData.AsEnumerable() where book.Field<string>("ISBN" + isbnLength) == isbn select book;
-
-			DataView result = bookInformation.AsDataView();
+        //Tikrinam isbn ar yra tokia knyga duombazej
 
 
-			//jei knyga rasta, tai atidarom langa su informacija apie ja, jei knyga nerasta praso ieskoti vel.
-			if (result.Count > 0)
-			{
-				this.Hide();
-				Book bookMenu = new Book(User, result);
-				bookMenu.ShowDialog();
+        public void CheckISBN(string isbn, int isbnLength)
+        {
+            //ieskom knygos su isbn
+            //	bookdata = SQLConnection.SelectQuery("SELECT * FROM Books WHERE ISBN" + isbnLength + " = '" + isbn + "';");
+            SQLConnection.AddISBNToHistory(User.Id, isbn);
+
+            DataTable bookData = GetAllBooksInDataTable();
+
+            var bookInformation = from book in bookData.AsEnumerable() where book.Field<string>("ISBN" + isbnLength) == isbn select book;
+
+            DataView result = bookInformation.AsDataView();
+
+
+            //jei knyga rasta, tai atidarom langa su informacija apie ja, jei knyga nerasta praso ieskoti vel.
+            if (result.Count > 0)
+            {
+                this.Hide();
+                Book bookMenu = new Book(User, result);
+                bookMenu.ShowDialog();
                 this.Close();
-			}
-			else
-			{
-				MessageBox.Show("We don't have this book at the moment, please try another one.");
-			}
-		}
+            }
+            else
+            {
+                MessageBox.Show("We don't have this book at the moment, please try another one.");
+            }
+        }
 
-		private void ScanButton_Click(object sender, EventArgs e)
+
+
+        private void ScanButton_Click(object sender, EventArgs e)
 		{
 			//skenuoja knygos barcode tuo metu, jei jo nepagauna, nemeta jokio erroro ir reik spaust vel
 			try
@@ -130,44 +107,33 @@ namespace VirtualLibraryApp
                 //System.Diagnostics.Process.Start("https://isbnsearch.org/isbn/" + result.Text);
 
                 //instantiate'inam delegata naudodami anonymous method'a
-                Check1013 ch1013 = delegate (string text, int length)
-                {
-                    if (length == 13)
-                    {
-                        CheckISBN(text, 13);
-                    }
-                    else if (length == 10)
-                    {
-                        CheckISBN(text, 10);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Wrong ISBN, please try again.");
-                    }
-                };
+                Check ch1013 = new Check(Check1013);
                 //iskvieciam delegata
-                ch1013(result.Text, result.Text.Length);
+                ch1013(result.Text.Length, result.Text);
 
-                //Tikrina ISBN formatą ar isbn13 ar isbn10, jei nei vienas, tai rastas blogas kodas.
-                //if (result.Text.Length == 13)
-                //{
-                //	CheckISBN(result.Text, 13);
-                //}
-                //else if (result.Text.Length == 10)
-                //{
-                //	CheckISBN(result.Text, 10);
-                //}
-                //else
-                //{
-                //	MessageBox.Show("Wrong ISBN, please try again.");
-                //}
             }
 			catch (MessagingToolkit.Barcode.NotFoundException)
 			{
 			}
 		}
 
-		private void CameraResolutionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        public void Check1013(int length, string text)
+        {
+            if (length == 13)
+            {
+                CheckISBN(text, 13);
+            }
+            else if (length == 10)
+            {
+                CheckISBN(text, 10);
+            }
+            else
+            {
+                MessageBox.Show("Wrong ISBN, please try again.");
+            }
+        }
+
+        private void CameraResolutionComboBox_SelectedIndexChanged(object sender, EventArgs e)
 		{
 			myCamera.Start(CameraResolutionComboBox.SelectedIndex);
 		}
