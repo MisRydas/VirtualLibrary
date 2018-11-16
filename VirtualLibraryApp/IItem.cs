d3e0b09328c13338387c23ab7c2e300562f7fd3f
Tomas Ukrinas <tomas.ukrinas1997@gmail.com>
2018-11-16 21:07:32 +0200

    Added Interface IItem

---
+++ VirtualLibraryApp/IItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace VirtualLibraryApp
+{
+    public interface IItem
+    {
+        String TableName();
+        String TypesInString();
+        String ValuesInString();
+    }
+}
