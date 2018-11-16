d35d5999bd7f6d443dcde99bda25c89120819371
Tomas Ukrinas <tomas.ukrinas1997@gmail.com>
2018-11-16 21:10:07 +0200

    Added class BookItem, which implements IItem. Also class User now Implements IItem

--- VirtualLibraryApp/User.cs
+++ VirtualLibraryApp/User.cs
@@ -6,12 +6,47 @@ using System.Threading.Tasks;
 
 namespace VirtualLibraryApp
 {
-    public struct User
+    public class User : IItem
     {
         public int Id { get; set; }
         public String UserName { get; set; }
         public String FirstName { get; set; }
         public String LastName { get; set; }
         public Boolean IsAdmin { get; set; }
+
+        public String Password { private get; set; }
+        public String TableName()
+        {
+            return "Users";
+        }
+        public String TypesInString()
+        {
+            return "(UserName, Password, FirstName, LastName)";
+        }
+        public String ValuesInString()
+        {
+            return String.Format("('{0}', '{1}', '{2}', '{3}')",
+                                        UserName, Password, FirstName, LastName);
+        }
+        public static Boolean operator ==(User left, User right)
+        {
+            if ((left.Id == right.Id) &&
+                (left.UserName == right.UserName) &&
+                (left.FirstName == right.FirstName) &&
+                (left.LastName == right.LastName) &&
+                (left.IsAdmin == right.IsAdmin))
+                return true;
+            else return false;
+        }
+        public static Boolean operator !=(User left, User right)
+        {
+            if ((left.Id == right.Id) &&
+                (left.UserName == right.UserName) &&
+                (left.FirstName == right.FirstName) &&
+                (left.LastName == right.LastName) &&
+                (left.IsAdmin == right.IsAdmin))
+                return false;
+            else return true;
+        }
     }
 }
