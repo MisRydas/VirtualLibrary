36dbfce1cbec26559b7f71adab2f42a6f05ae851
Tadynas <griciustadas@gmail.com>
2018-11-25 20:18:36 +0200

    Created interface folder and added new script ILogin

---
+++ Logic/Interfaces/ILogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+	interface ILogin
+	{
+		event Action ButtonPressed;
+
+		String Username { get; }
+		String Password { get; }
+		void OnError(string message);
+	}
+}
