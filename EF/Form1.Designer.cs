a93ee13310416e70799744f663ff583ce365b892
Efkinatorius <36881430+Efkinatorius@users.noreply.github.com>
2018-12-18 01:02:45 +0200

    ef

--- EF/Form1.Designer.cs
+++ EF/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.commentTextBox = new System.Windows.Forms.TextBox();
+            this.commentButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // commentTextBox
+            // 
+            this.commentTextBox.Location = new System.Drawing.Point(44, 61);
+            this.commentTextBox.Multiline = true;
+            this.commentTextBox.Name = "commentTextBox";
+            this.commentTextBox.Size = new System.Drawing.Size(224, 88);
+            this.commentTextBox.TabIndex = 0;
+            // 
+            // commentButton
+            // 
+            this.commentButton.Location = new System.Drawing.Point(113, 185);
+            this.commentButton.Name = "commentButton";
+            this.commentButton.Size = new System.Drawing.Size(75, 23);
+            this.commentButton.TabIndex = 1;
+            this.commentButton.Text = "Comment";
+            this.commentButton.UseVisualStyleBackColor = true;
+            this.commentButton.Click += new System.EventHandler(this.commentButton_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Comment});
+            this.dataGridView.Location = new System.Drawing.Point(388, 41);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(369, 309);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // Comment
+            // 
+            this.Comment.DataPropertyName = "Comment";
+            this.Comment.HeaderText = "Comment";
+            this.Comment.Name = "Comment";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.commentButton);
+            this.Controls.Add(this.commentTextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox commentTextBox;
+        private System.Windows.Forms.Button commentButton;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comment;
     }
 }
 
--- EF/Form1.cs
+++ EF/Form1.cs
@@ -12,9 +12,47 @@ namespace EF
 {
     public partial class Form1 : Form
     {
+        Table model = new Table();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void commentButton_Click(object sender, EventArgs e)
+        {
+            //var context = new CommentDBEntities();
+            //var table = new Table()
+            //{
+            //    comment = commentTextBox.Text
+            //};
+
+            //context.Tables.Add(table);
+            //context.SaveChanges();
+            model.comment = commentTextBox.Text.Trim();
+            using (CommentDBEntities db = new CommentDBEntities())
+            {
+                //if (model.Id == 0) db.Tables.Add(model);
+                //else db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.Tables.Add(model);
+                db.SaveChanges();
+            }
+            Clear();
+            FillDataGridView();
+
+        }
+
+        void Clear()
+        {
+            commentTextBox.Text = "";
+            model.Id = 0;
+        }
+
+        void FillDataGridView()
+        {
+            using (CommentDBEntities db = new CommentDBEntities())
+            {
+                dataGridView.DataSource = db.Tables.ToList<Table>();
+            }
         }
     }
 }
