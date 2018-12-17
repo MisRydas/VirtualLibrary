019b368fe8fe56f0ccf7a071908773ab313abd0d
Tadynas <griciustadas@gmail.com>
2018-12-17 22:29:34 +0200

    Updated add book tests.

--- VirtualLibraryTests/AddBookTests.cs
+++ VirtualLibraryTests/AddBookTests.cs
@@ -69,5 +69,65 @@ namespace VirtualLibraryTests
 			//Assert
 			Assert.AreEqual(error, possibleError);
 		}
+
+		[TestMethod]
+		public void CheckAddBook_WithWrongDate_ReturnError1()
+		{
+			//Arrange
+			string name = "name";
+			string isbn13 = "0123456789123";
+			string isbn10 = "0123456789";
+			string author = "author";
+			string publisher = "publisher";
+			string genre = "genre";
+			string published = "3333";
+			string listPrice = "9.99";
+			string coverLink = "google.com";
+			string possibleError = "Wrong Published Date format. Example: 2018\n";
+			//Act
+			addbook.Add(ref error, name, isbn13, isbn10, author, publisher, genre, published, listPrice, coverLink);
+			//Assert
+			Assert.AreEqual(error, possibleError);
+		}
+
+		[TestMethod]
+		public void CheckAddBook_WithWrongDate_ReturnError2()
+		{
+			//Arrange
+			string name = "name";
+			string isbn13 = "0123456789123";
+			string isbn10 = "0123456789";
+			string author = "author";
+			string publisher = "publisher";
+			string genre = "genre";
+			string published = "0000";
+			string listPrice = "9.99";
+			string coverLink = "google.com";
+			string possibleError = "Wrong Published Date format. Example: 2018\n";
+			//Act
+			addbook.Add(ref error, name, isbn13, isbn10, author, publisher, genre, published, listPrice, coverLink);
+			//Assert
+			Assert.AreEqual(error, possibleError);
+		}
+
+		[TestMethod]
+		public void CheckAddBook_WithWrongPrice_ReturnError()
+		{
+			//Arrange
+			string name = "name";
+			string isbn13 = "0123456789123";
+			string isbn10 = "0123456789";
+			string author = "author";
+			string publisher = "publisher";
+			string genre = "genre";
+			string published = "2018";
+			string listPrice = "2.9ka";
+			string coverLink = "google.com";
+			string possibleError = "Wrong ListPrice format\n";
+			//Act
+			addbook.Add(ref error, name, isbn13, isbn10, author, publisher, genre, published, listPrice, coverLink);
+			//Assert
+			Assert.AreEqual(error, possibleError);
+		}
 	}
 }
