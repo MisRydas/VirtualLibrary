c8ce77dd9f150d7e260b158ca0c6aefc2c5bb13f
Tadynas <griciustadas@gmail.com>
2018-12-17 21:53:47 +0200

    Added Unit tests to xamarin. Added Login check tests.

---
+++ VirtualLibraryTests/LoginTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using LogicWebService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VirtualLibraryTests
+{
+	[TestClass]
+	public class LoginTests
+	{
+		[TestMethod]
+		public void Login_WithValidInput_ReturnTrue()
+		{
+			//Arrange
+			Login login = new Login();
+			string username = "admin";
+			string password = "administrator";
+			string error = "";
+			bool isAdmin = false;
+			//Act
+			bool result = login.LoginCheck(username, password,ref error, ref isAdmin);
+			//Assert
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void Login_WithNonValidInput_ReturnFalse()
+		{
+			//Arrange
+			Login login = new Login();
+			string username = "ad";
+			string password = "ad";
+			string error = "";
+			bool isAdmin = false;
+			//Act
+			bool result = login.LoginCheck(username, password, ref error, ref isAdmin);
+			//Assert
+			Assert.IsFalse(result);
+		}
+
+		[TestMethod]
+		public void Login_WithAdminInput_ReturnIsAdminTrue()
+		{
+			//Arrange
+			Login login = new Login();
+			string username = "admin";
+			string password = "administrator";
+			string error = "";
+			bool isAdmin = false;
+			//Act
+			bool result = login.LoginCheck(username, password, ref error, ref isAdmin);
+			//Assert
+			Assert.IsTrue(isAdmin);
+		}
+
+		[TestMethod]
+		public void Login_WithNonAdminInput_ReturnIsAdminFalse()
+		{
+			//Arrange
+			Login login = new Login();
+			string username = "yo";
+			string password = "yo";
+			string error = "";
+			bool isAdmin = false;
+			//Act
+			bool result = login.LoginCheck(username, password, ref error, ref isAdmin);
+			//Assert
+			Assert.IsFalse(isAdmin);
+		}
+
+		[TestMethod]
+		public void Login_WithNonValidInput_ReturnIsWrongCreditials()
+		{
+			//Arrange
+			Login login = new Login();
+			string username = "ad";
+			string password = "ad";
+			string possibleError = "Wrong username or password";
+			string error = "";
+			bool isAdmin = false;
+			//Act
+			bool result = login.LoginCheck(username, password, ref error, ref isAdmin);
+			//Assert
+			Assert.AreEqual(error, possibleError);
+		}
+	}
+}
---
+++ VirtualLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VirtualLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VirtualLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a4c1dd7-1266-47d0-9835-3dae6637ccfd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
